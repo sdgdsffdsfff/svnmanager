d90e820c0b624584ca825c0a06fd689414470d17
languid <languid.jeremy@gmail.com>
2015-03-17 16:18:57 +0800

    commit

--- test.go
+++ test.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-"fmt"
 sh "github.com/codeskyblue/go-sh"
 )
 
 func main() {
 	session := sh.NewSession()
-	err := session.SetDir("/opt/wings").Command("mvn clean:clean compile").Run()
+	session.SetDir("/opt/wings").Command("mvn clean:clean compile").Run()
 }
