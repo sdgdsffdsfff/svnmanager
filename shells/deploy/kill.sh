f05631ddc25b10de55092c9f8f45f05ce8f2dc66
languid <languid.jeremy@gmail.com>
2015-03-20 11:56:30 +0800

    commit

--- shells/deploy/kill.sh
+++ shells/deploy/kill.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-isError=false;
 times=0;
 while [ `ps aux|grep java | wc -l` -gt 1 ]
 do
-    sh /usr/local/tomcat6/bin/shutdown.sh || isError=true && break
+    sh /usr/local/tomcat6/bin/shutdown.sh
     sleep 6
     times=$[$times+1]
     if [[ $times -ge 5 && `ps aux|grep java | wc -l` -gt 1 ]]
     then
         ps aux | grep java | awk '{f++;if(NF>12){id=$2}} END {print "kill -9 " id}' |sh
     fi
-    isError=false
 done
 
-if [ isError ]
-then
-    echo "error"
-else
-    echo "complete"
-fi
+echo "complete"
--- shells/test.sh
+++ shells/test.sh
@@ -1,3 +1,14 @@
-#!/usr/bin/env bash
-sleep 10
-echo "aa"
+#!/bin/bash
+
+a=1;
+while [ true ]
+do
+    if [ $a -gt 5 ]
+    then
+        cd /home/aaa || a=10 && break
+    else
+        a=6
+    fi
+done
+
+echo $a
